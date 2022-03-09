c6d083f89722979c41cc9d8050cb6c427e2da368
Yunke <45084733+naivety77@users.noreply.github.com>
2022-03-09 15:43:21 +0800

    Add files via upload

---
+++ mujoco/models.py
@@ -0,0 +1,124 @@
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import deepdish as dd
+
+
+class Policy(nn.Module):
+    def __init__(self, num_inputs, num_outputs, hidden_size):
+        super(Policy, self).__init__()
+        self.affine1 = nn.Linear(num_inputs, hidden_size)
+        self.affine2 = nn.Linear(hidden_size, hidden_size)
+
+        self.action_mean = nn.Linear(hidden_size, num_outputs)
+        self.action_mean.weight.data.mul_(0.1)
+        self.action_mean.bias.data.mul_(0.0)
+
+        self.action_log_std = nn.Parameter(torch.zeros(1, num_outputs))
+
+        self.saved_actions = []
+        self.rewards = []
+        self.final_value = 0
+
+    def forward(self, x):
+        x = F.tanh(self.affine1(x))
+        x = F.tanh(self.affine2(x))
+
+        action_mean = self.action_mean(x)
+        action_log_std = self.action_log_std.expand_as(action_mean)
+        action_std = torch.exp(action_log_std)
+
+        return action_mean, action_log_std, action_std
+
+
+class Value(nn.Module):
+    def __init__(self, num_inputs, hidden_size):
+        super(Value, self).__init__()
+        self.affine1 = nn.Linear(num_inputs, hidden_size)
+        self.affine2 = nn.Linear(hidden_size, hidden_size)
+        self.value_head = nn.Linear(hidden_size, 1)
+        self.value_head.weight.data.mul_(0.1)
+        self.value_head.bias.data.mul_(0.0)
+
+    def forward(self, x):
+        x = F.tanh(self.affine1(x))
+        x = F.tanh(self.affine2(x))
+
+        state_values = self.value_head(x)
+        return state_values
+
+
+class Discriminator(nn.Module):
+    def __init__(self, num_inputs, hidden_size):
+        super(Discriminator, self).__init__()
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, 1)
+        self.linear3.weight.data.mul_(0.1)
+        self.linear3.bias.data.mul_(0.0)
+
+    def forward(self, x):
+        x = F.tanh(self.linear1(x))
+        x = F.tanh(self.linear2(x))
+        #prob = F.sigmoid(self.linear3(x))
+        output = self.linear3(x)
+        return output
+
+
+class Generator(nn.Module):
+    def __init__(self, num_inputs, hidden_size, num_outputs):
+        super(Generator, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, num_outputs)
+
+    def forward(self, x):
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class Classifier(nn.Module):
+    def __init__(self, num_inputs, hidden_dim):
+        super(Classifier, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, 1)
+
+        self.d1 = nn.Dropout(0.5)
+        self.d2 = nn.Dropout(0.5)
+        
+        self.fc3.weight.data.mul_(0.1)
+        self.fc3.bias.data.mul_(0.0)
+
+    def forward(self, x):
+        x = self.d1(torch.tanh(self.fc1(x)))
+        x = self.d2(torch.tanh(self.fc2(x)))
+        x = self.fc3(x)
+        return x
+
+
+class Reward(nn.Module):
+    def __init__(self, num_inputs, hidden_size=256):
+        super(Reward, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def init_weight(self, model_path):
+        weights = dd.io.load(model_path)
+        self.fc1.weight.data = torch.from_numpy(weights['fc1_weight'])
+        self.fc1.bias.data = torch.from_numpy(weights['fc1_bias'])
+        self.fc2.weight.data = torch.from_numpy(weights['fc2_weight'])
+        self.fc2.bias.data = torch.from_numpy(weights['fc2_bias'])
+        self.fc3.weight.data = torch.from_numpy(weights['fc3_weight'])
+        self.fc3.bias.data = torch.from_numpy(weights['fc3_bias'])
+
---
+++ mujoco/running_state.py
@@ -0,0 +1,98 @@
+from collections import deque
+import numpy as np
+
+# from https://github.com/joschu/modular_rl
+# http://www.johndcook.com/blog/standard_deviation/
+class RunningStat(object):
+    def __init__(self, shape):
+        self._n = 0
+        self._M = np.zeros(shape)
+        self._S = np.zeros(shape)
+
+    def push(self, x):
+        x = np.asarray(x)
+        assert x.shape == self._M.shape
+        self._n += 1
+        if self._n == 1:
+            self._M[...] = x
+        else:
+            oldM = self._M.copy()
+            self._M[...] = oldM + (x - oldM) / self._n
+            self._S[...] = self._S + (x - oldM) * (x - self._M)
+
+    @property
+    def n(self):
+        return self._n
+
+    @property
+    def mean(self):
+        return self._M
+
+    @property
+    def var(self):
+        return self._S / (self._n - 1) if self._n > 1 else np.square(self._M)
+
+    @property
+    def std(self):
+        return np.sqrt(self.var)
+
+    @property
+    def shape(self):
+        return self._M.shape
+
+
+class ZFilter:
+    """
+    y = (x-mean)/std
+    using running estimates of mean,std
+    """
+    def __init__(self, shape, demean=True, destd=True, clip=10.0):
+        self.demean = demean
+        self.destd = destd
+        self.clip = clip
+
+        self.rs = RunningStat(shape)
+
+    def __call__(self, x, update=True):
+        if update: self.rs.push(x)
+        if self.demean:
+            x = x - self.rs.mean
+        if self.destd:
+            x = x / (self.rs.std + 1e-8)
+        if self.clip:
+            x = np.clip(x, -self.clip, self.clip)
+        return x
+
+    def output_shape(self, input_space):
+        return input_space.shape
+
+
+class RunningMeanStd(object):
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(self, epsilon=1e-8, shape=()):
+        self.mean = np.zeros(shape, 'float64')
+        self.var = np.ones(shape, 'float64')
+        self.count = epsilon
+
+    def update(self, x):
+        batch_mean = np.mean(x, axis=0)
+        batch_var = np.var(x, axis=0)
+        batch_count = x.shape[0]
+        self.update_from_moments(batch_mean, batch_var, batch_count)
+
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        self.mean, self.var, self.count = update_mean_var_count_from_moments(
+            self.mean, self.var, self.count, batch_mean, batch_var, batch_count)
+
+def update_mean_var_count_from_moments(mean, var, count, batch_mean, batch_var, batch_count):
+    delta = batch_mean - mean
+    tot_count = count + batch_count
+
+    new_mean = mean + delta * batch_count / tot_count
+    m_a = var * count
+    m_b = batch_var * batch_count
+    M2 = m_a + m_b + np.square(delta) * count * batch_count / tot_count
+    new_var = M2 / tot_count
+    new_count = tot_count
+
+    return new_mean, new_var, new_count
